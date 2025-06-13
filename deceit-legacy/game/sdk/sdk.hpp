f826493e3c4295b2ab08f30df73ca7f65f222ce8
dm1triisolovev <thesomeguynextdoor@gmail.com>
2025-06-13 13:15:21 +0300

    [add/delete] UObject functino(ProcessEvent | StaticFindObject | FindObject | FindObjectUClass) | offsets.hpp(no need)

--- deceit-legacy/game/sdk/sdk.hpp
+++ deceit-legacy/game/sdk/sdk.hpp
@@ -1,1 +1,54 @@
 #pragma once
+
+#include "Windows.h"
+
+#define FUNCTION_TYPE( type, name, address, ... ) \
+		using fn = type(*)( __VA_ARGS__ ); \
+		const auto fn_##name = reinterpret_cast<fn>( address )
+
+namespace sdk
+{
+	struct u_class_t{};
+	struct u_object_t
+	{
+		__forceinline static __int64* u_class_find_object( void* _outer, const wchar_t* _name )
+		{
+			const auto base = reinterpret_cast< uintptr_t >( GetModuleHandleA( nullptr ) );
+
+			FUNCTION_TYPE( __int64*, u_class_find_object_ptr, base + 0x12af2f0, void*, const wchar_t* );
+			if ( !fn_u_class_find_object_ptr )
+				return nullptr;
+
+			return fn_u_class_find_object_ptr( _outer, _name );
+		}
+
+		template<typename T>
+		__forceinline static T find_object( const wchar_t* _name, u_object_t* _outer = nullptr )
+		{
+			return static_find_object<T>( nullptr, _outer, _name );
+		}
+
+		template<typename T>
+		__forceinline static T static_find_object(u_object_t* _this, u_object_t* _outer, const wchar_t* _name )
+		{
+			const auto base = reinterpret_cast< uintptr_t >( GetModuleHandleA( nullptr ) );
+
+			FUNCTION_TYPE( T, static_find_object_ptr, base + 0x1472910, u_object_t*, u_object_t*, const wchar_t* );
+			if ( !fn_static_find_object_ptr )
+				return nullptr;
+
+			return fn_static_find_object_ptr( _this, _outer, _name );
+		}
+
+		__forceinline void process_event(u_object_t* _func, void* _args )
+		{
+			const auto base = reinterpret_cast< uintptr_t >( GetModuleHandleA( nullptr ) );
+
+			FUNCTION_TYPE( void, process_event_ptr, base + 0x14685c0, u_object_t*, u_object_t*, void* );
+			if ( !fn_process_event_ptr )
+					return nullptr;
+
+			return fn_process_event_ptr( this, _func, _args );
+		}
+	};
+}
